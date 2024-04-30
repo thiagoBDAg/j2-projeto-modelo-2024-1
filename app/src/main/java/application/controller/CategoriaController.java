7ce6d32f891a2d8f7e6ea440fa1ee3e4ac933538
marcoweb <marcocarvalho.web@gmail.com>
2024-04-30 16:43:10 +0000

    .

--- app/src/main/java/application/controller/CategoriaController.java
+++ app/src/main/java/application/controller/CategoriaController.java
@@ -1,9 +1,11 @@
 package application.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import application.model.Categoria;
 import application.repository.CategoriaRepository;
 
 @RestController
@@ -12,5 +14,8 @@ public class CategoriaController {
     @Autowired
     private CategoriaRepository categoriaRepo;
 
-    
+    @GetMapping
+    public Iterable<Categoria> getAll() {
+        return categoriaRepo.findAll();
+    }
 }
