8aed1f1b968ecd408715e78eacfba3c5cfdae674
marcoweb <marcocarvalho.web@gmail.com>
2024-04-30 17:07:34 +0000

    .

--- app/src/main/java/application/controller/CategoriaController.java
+++ app/src/main/java/application/controller/CategoriaController.java
@@ -1,11 +1,16 @@
 package application.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import application.model.Categoria;
 import application.repository.CategoriaRepository;
@@ -19,6 +24,17 @@ public class CategoriaController {
     @GetMapping
     public Iterable<Categoria> getAll() {
         return categoriaRepo.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Categoria getOne(@PathVariable long id) {
+        Optional<Categoria> result = categoriaRepo.findById(id);
+        if(result.isEmpty()) {
+            throw new ResponseStatusException(
+                HttpStatus.NOT_FOUND, "Categoria Não Encontrada"
+            );
+        }
+        return result.get();
     }
 
     @PostMapping
