edb97e957081ae84260bee29692c828cfe919cd4
marcoweb <marcocarvalho.web@gmail.com>
2024-05-07 17:10:10 +0000

    TODO: Opçoes fetch

--- app/src/main/java/application/controller/OpcaoController.java
+++ app/src/main/java/application/controller/OpcaoController.java
@@ -11,12 +11,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import application.model.Opcao;
 import application.repository.OpcaoRepository;
 import application.repository.QuestaoRepository;
 
+@RestController
 @RequestMapping("/opcoes")
 public class OpcaoController {
     @Autowired
@@ -41,7 +43,7 @@ public class OpcaoController {
         return result.get();
     }
 
-    @DeleteMapping("{/id}")
+    @DeleteMapping("/{id}")
     public void delete(@PathVariable long id) {
         if(opcoesRepo.existsById(id)) {
             opcoesRepo.deleteById(id);
--- app/src/main/java/application/model/Questao.java
+++ app/src/main/java/application/model/Questao.java
@@ -1,12 +1,17 @@
 package application.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,6 +26,9 @@ public class Questao {
     @ManyToOne
     @JoinColumn(name = "id_categoria", nullable = false)
     private Categoria categoria;
+
+    @OneToMany(mappedBy = "questao")
+    private Set<Opcao> opcoes = new HashSet<Opcao>();
 
     public long getId() {
         return id;
@@ -39,5 +47,11 @@ public class Questao {
     }
     public void setCategoria(Categoria categoria) {
         this.categoria = categoria;
+    }
+    public Set<Opcao> getOpcoes() {
+        return opcoes;
+    }
+    public void setOpcoes(Set<Opcao> opcoes) {
+        this.opcoes = opcoes;
     }
 }
