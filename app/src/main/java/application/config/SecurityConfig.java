54bad3f55add1a2fe3bdb3b161b076af734d4230
marcoweb <marcocarvalho.web@gmail.com>
2024-05-13 17:36:54 +0000

    .

---
+++ app/src/main/java/application/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package application.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http) throws Exception {
+                return http
+                    .csrf().disable()
+                    .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                    .and().authorizeHttpRequests()
+                    .requestMatchers(HttpMethod.POST, "/auth").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                    .build();
+    }
+
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration)
+        throws Exception {
+            
+        }
+}
