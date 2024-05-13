938054d0175b6e3893022f6e8dddfa6833a6ef73
marcoweb <marcocarvalho.web@gmail.com>
2024-05-13 17:40:07 +0000

    .

--- app/src/main/java/application/config/SecurityConfig.java
+++ app/src/main/java/application/config/SecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -27,8 +29,14 @@ public class SecurityConfig {
                     .build();
     }
 
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration)
-        throws Exception {
-            
-        }
+    @Bean
+    public AuthenticationManager authenticationManager(
+        AuthenticationConfiguration configuration) throws Exception {
+            return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
